cf88c7c850aa0ae611d702ed9461ec362a7fedcf
dayanar23 <dayana2301@gmail.com>
2016-03-05 22:00:53 -0430

    primeras funcionalidades de las estrategias

--- rps.rb
+++ rps.rb
@@ -4,127 +4,173 @@
 # Dayana Rodrigues 10-10615
 # Roberto Romero   10-10642
 
-
-# to_s muestra el invocante como un String.
-# score(m) determina el resultado de la jugada entre el invocante y el movimiento
-#m, correspondiente al contrincante, que es recibido como argumento. 
-#El resultado de score debe ser una tupla que representa la ganancia en puntos resultado de la jugada:
-#el primer elemento de la tupla representa la ganancia del invocante, mientras que el
-#segundo elemento representa la ganancia del contrincante. As ́ı, la tupla resultante debe
-#ser [1,0], [0,1] o [0,0] dependiendo de los movimientos involucrados.
-
 class Movement
-	def initialize x
-                @x = x
-        end
-        def to_s
-                self.class.name
-        end	
+	def to_s
+		self.class.name
+	end		
 end
 
 class Rock < Movement
-	def initialize x
-                super x
-        end
-        def score m
-                m.scoreRock(self)
-        end
-        def scoreRock m
-                [0,0] # empate
-        end
-        def scorePaper m
-                [1,0] # gana el invocante
-        end
-        def scoreScissors m
-                [0,1] # gana rock
-        end
+	def score m
+		m.scoreRock(self)
+	end
+	def scoreRock m
+		[0,0] # empate
+	end
+	def scorePaper m
+		[1,0] # gana el invocante
+	end
+	def scoreScissors m
+		[0,1] # gana rock
+	end
 end
 
 class Paper < Movement
-	def initialize x
-                super x
-        end
-        def score m
-                m.scorePaper(self)
-        end
-        def scoreRock m
-                [0,1] # gana paper
-        end
-        def scorePaper m
-                [0,0] # empate
-        end
-        def scoreScissors m
-                [1,0] # gana el invocante
-        end
+	def score m
+		m.scorePaper(self)
+	end
+	def scoreRock m
+		[0,1] # gana paper
+	end
+	def scorePaper m
+		[0,0] # empate
+	end
+	def scoreScissors m
+		[1,0] # gana el invocante
+	end
 end
 
 class Scissors < Movement
-	def initialize x
-                super x
-        end
-        def score m
-                m.scoreScissors(self)
-        end
-        def scoreRock m
-                [1,0] # gana el invocante
-        end
-        def scorePaper m
-                [0,1] # gana scissors
-        end
-        def scoreScissors m
-                [0,0] # empate
-        end
+	def score m
+		m.scoreScissors(self)
+	end
+	def scoreRock m
+		[1,0] # gana el invocante
+	end
+	def scorePaper m
+		[0,1] # gana scissors
+	end
+	def scoreScissors m
+		[0,0] # empate
+	end
 end
 
 ##### clase jugador
-class Strategy 
-	def initialize x
-                @x = x
-        end
-        def next(m)
-                # genera el proxio elemeno
-        end
-        def to_s
-                self.class.name
-                # debe estar especializada para mostar los 
-                # parametros de configuracios de cada estrategia
-        end
-        def reset
-                # lleva la estrategia al estado inicial
-        end
+class Strategy
+	SEED = 42
+	def initialize
+		# raise lanza el mensaje de error :D 
+		raise 'Strategy is an abstract class'
+	end
+	# SERA QUE PONGO ERROR EN CADA METODO? 
 end
 
 class Uniform < Strategy
-        def initialize x
-                super x
-                # debe eliminar duplicados y la lista no debe estar
-                # vacia
-        end
-        # debe seleccionar elemento usando una distribucion uniforme
-        # preguntar que coño significa eso
+
+	# constructor de la clase
+	def initialize list
+	# si la lista está vacía muestra error
+		if list.empty?
+			raise 'Empty list'
+		else	
+			# uniq elimina los elementos repetidos
+			# compact elimina los nil del array
+           	@list = list.compact.uniq
+		end
+	end
+	
+	# para poder trabajar con la lista, MOSCA falta el atrr_algo
+	def list
+		@list
+	end
+
+	# devuelve el proximo movimiento
+	def next(m)
+		# gen es el generador de aleatoriedad
+		gen 	= Random.new(SEED)
+		# posicion del elemento a seleccionar
+		index 	= gen.rand(self.list.size)
+		# movimiento resultante
+		move	= self.list[index]
+		return move 
+	end
+
+	# muestra el nombre de la estragia y sus atributos
+	def to_s
+		self.class.name + " " + self.list.to_s
+	end
+
+	# vuelve la estrategia al estado inicial
+	def reset
+		# si vamos a seetear el estado inicial, aqui lo seteamos de nuevo
+	end
+
 end
 
 class Biased < Strategy
-        def initialize x
-                super x
-                #eliminar duplicados y verificar que el hash no este vacio
-        end
-        # la clave son objetos de clase Movement, y los valores son 
-        # enteros que representan las probabilidades de cada clave
+
+	# constructor de la clase
+	def initialize hash
+		# si el has esta vacio
+		if hash.empty?
+			raise 'Empty hash'
+		else
+			# uniq puede recibir un bloque y así elimina claves repetidas
+			@hash = hash.uniq { |x| x.key}
+		end
+	end
+
+	# devuelve el proximo movimiento
+	def next(m)
+	end
+	def to_s
+	end
+	def reset
+	end
+	# la clave son objetos de clase Movement, y los valores son 
+    # enteros que representan las probabilidades de cada clave
 end
 
 class Mirror < Strategy
-        def initialize x
-                super x
-        end
-        #la primera jugada se setea al construirse, las demas son la
-        # jugada anterior del contrincante
+
+	# ATRR TAMBIEN
+	# constructor de la clase
+	def initialize (constructor, other_moves = [])
+		@constructor = constructor
+		# movimientos anteriores del contrincante
+		@other_moves = other_moves		
+	end
+
+	# devuelve el proximo elemento
+	def next(m)
+		if other_moves.empty?
+			mov = self.constructor
+		else
+			mov = other_moves.last
+		end
+		other_moves.insert(m)
+		return mov
+	end
+	def to_s
+	end
+	def reset
+	end
+	#la primera jugada se setea al construirse, las demas son la
+	# jugada anterior del contrincante
 end
 
+
 class Smart < Strategy
-        def initialize x
-                super x
-        end
+	def initialize x
+		super x
+	end
+	def next(m)
+	end
+	def to_s
+	end
+	def reset
+	end
+
         #analiza las frecuencias de las jugadas del oponente
         #debe recordad las jugadas aneriores y decidir segun:
                 # P,R,S la cantidad de rock, paper, scissors
@@ -136,10 +182,7 @@ end
 
 #### clase juego
 class Match
-	def initialize(args)
-		
+	def initialize(args)	
 	end
-	
-	
 end
 
